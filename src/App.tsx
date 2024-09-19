105365bcae52234bb65d9132d9fcdf80aaa58a37
Peangelo <peangelorafa@gmail.com>
2024-09-19 10:18:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@mui/material';
+import './App.css';
+import Cabecalho from './components/Cabecalho';
+
+function App() {
+  return (
+    <>
+      <Cabecalho/>
+      <Container></Container>
+    </>
+  );
+}
+
+export default App;
